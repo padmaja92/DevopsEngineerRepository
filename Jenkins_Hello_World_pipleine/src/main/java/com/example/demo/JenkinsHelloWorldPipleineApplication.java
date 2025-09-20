d5e54728883d46fb615d63ddda2d32e09b65121f
padmaja92 <amaradhi.padmaja@gmail.com>
2025-09-20 17:09:34 -0500

    Adding First Java project to build on jenkins

---
+++ Jenkins_Hello_World_pipleine/src/main/java/com/example/demo/JenkinsHelloWorldPipleineApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class JenkinsHelloWorldPipleineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsHelloWorldPipleineApplication.class, args);
+	}
+
+	@GetMapping("/GetWelcomePage")
+	public String welcomepage() {
+		return  "Welcome page with run multi agent and multi staged Jenkins project" ;
+	}
+
+}
